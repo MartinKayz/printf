f1b60957021472a735085c91cc0041f3aef77c27
Earnest <ebaguma1@gmail.com>
2022-10-17 22:49:36 +0300

    Correcting the main.h header file

--- main.h
+++ main.h
@@ -1,9 +1,22 @@
 # ifndef _MAIN_H
 # define _MAIN_H
+#include <stdarg.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_nbr(va_list ap);
 
+/**
+ * struct flags_printf - struct conversion to function
+ * @c: flag string
+ * @f: pointer to func
+ */
 
+typedef struct flags_printf
+{
+	char *c;
+	int (*f)(va_list);
+} flags_p;
+#endif
 
-
-
-
-# endif
