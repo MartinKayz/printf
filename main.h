5162fcc5235e74a90fd7156b9de5c4d96eb9cfd6
MartinKays <kubonamartinyafesi132@gmail.com>
2022-10-18 23:11:15 +0300

    Updated header

--- main.h
+++ main.h
@@ -1,10 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <stdarg.h>
 int _printf(const char *format, ...);
 int print_char(va_list ap);
 int print_str(va_list ap);
+int print_nbr(va_list ap);
+int print_binary(va_list ap);
+int print_octal(va_list ap);
+int print_hexa_lower(va_list ap);
+int print_hexa_upper(va_list ap);
+int print_unsigned(va_list ap);
+int print_str_unprintable(va_list ap);
+int print_str_reverse(va_list ap);
+int print_ptr(va_list ap);
 int print_rot13(va_list ap);
 int print_percent(va_list ap __attribute__((unused)));
 int print_number(int n);
